ca9708cbdd0b93d0ef12929fedd3061b665f3840
Destoky <adestoky@gmail.com>
2021-09-10 12:10:01 +0200

    Header et GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import { GlobalStyle } from './GlobalStyle';
+import Header from './components/Header/Header';
+
 function App() {
   return (
     <div className="App">
       Start here.
+      <GlobalStyle/>
+      <Header/>
     </div>
   );
 }
