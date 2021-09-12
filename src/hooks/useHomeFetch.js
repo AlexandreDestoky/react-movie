42f79c66d6a645eb0d16eb4dc32beeb9ecdd8b0a
Destoky <adestoky@gmail.com>
2021-09-12 22:38:41 +0200

    useHomeFetch InitialState

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -1,13 +1,20 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from 'react';
+// API
+import API from '../API';
 
-import API from "../API";
+const initialState = {
+  page: 0,
+  results: [],
+  total_pages: 0,
+  total_results: 0
+};
 
-export default function useHomeFetch() {
+export const useHomeFetch = () => {
   const [state, setState] = useState(initialState);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  const fetchMovies = async (page, searchTerm = "") => {
+  const fetchMovies = async (page, searchTerm = '') => {
     try {
       setError(false);
       setLoading(true);
@@ -16,7 +23,8 @@ export default function useHomeFetch() {
 
       setState(prev => ({
         ...movies,
-        results: page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
       }));
     } catch (error) {
       setError(true);
@@ -30,4 +38,4 @@ export default function useHomeFetch() {
   }, []);
 
   return { state, loading, error };
-}
+};
