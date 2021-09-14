320558f20f90ddcca4bc716ea5d75943bc31be6f
Destoky <adestoky@gmail.com>
2021-09-14 11:30:54 +0200

    HeroImage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import { useState,useEffect } from 'react';
-import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
-import NoImage from '../images/no_image.jpg';
-
+import React from "react";
+import { useState, useEffect } from "react";
+import { useHomeFetch } from "../hooks/useHomeFetch";
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
+import NoImage from "../images/no_image.jpg";
+import HeroImage from "./HeroImage/HeroImage";
 
 export default function Home() {
   const { state, loading, error } = useHomeFetch();
   return (
-    <div>
-      <p>Home</p>
-    </div>
-  )
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  );
 }
-
-
